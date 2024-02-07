de7b363cc0d0906e5a9e949040a6a10c520f29ce
bertua <151688160+bertua@users.noreply.github.com>
2024-02-07 03:33:42 +0000

    join

---
+++ lucas/Args/IntercalarJoin.java
@@ -0,0 +1,7 @@
+public class IntercalarJoin{
+    public static void main(String[] args){
+        String a = String.join("-", args);
+        System.out.print(a);
+        System.out.println();
+    }
+}
---
+++ lucas/Args/TestArgs.java
@@ -0,0 +1,6 @@
+import java.util.Arrays;
+public class TestArgs{
+    public static void main(String[] args){
+        System.out.println(Arrays.deepToString(args));
+    }
+}
