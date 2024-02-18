dac3d12f0ef8411bee188527e496a5193f4a9152
bertua <151688160+bertua@users.noreply.github.com>
2024-02-18 17:37:35 -0300

    Update Ex10.java

--- lucas/Strings/Ex10.java
+++ lucas/Strings/Ex10.java
@@ -1,7 +1,7 @@
 public class Ex10 {
     public static void main(String[] args){
         String camelCase = "";
-        System.out.print(args[0]);
+        System.out.print(args[0].toLowerCase);
         for(int i = 1; i < args.length; i++){
             camelCase = args[i].substring(0,1).toUpperCase() + args[i].substring(1);
             System.out.print(camelCase);
