efd53f72ac8f8c725cddac9d9a628973c2e844a3
bertua <151688160+bertua@users.noreply.github.com>
2024-02-18 17:39:56 -0300

    Update Ex07.java

--- lucas/Strings/Ex07.java
+++ lucas/Strings/Ex07.java
@@ -2,7 +2,7 @@ public class Ex07 {
     public static void main(String[] args){
         String title = "";
         for(int i = 0; i < args.length; i++){
-            title = args[i].substring(0,1).toUpperCase() + args[i].substring(1);
+            title = args[i].substring(0,1).toUpperCase() + args[i].substring(1).toLowerCase();
             System.out.print(title);
             if(i < args.length -1){
                 System.out.print(" ");
