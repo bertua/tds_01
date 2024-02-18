4809ff2e8b986a410e5c1bad482b870dbacefcc1
bertua <151688160+bertua@users.noreply.github.com>
2024-02-18 17:41:27 -0300

    Update Ex11.java

--- lucas/Strings/Ex11.java
+++ lucas/Strings/Ex11.java
@@ -2,7 +2,7 @@ public class Ex11 {
     public static void main(String[] args){
         String PascalCase = "";
         for(int i = 0; i < args.length; i++){
-            PascalCase = args[i].substring(0,1).toUpperCase() + args[i].substring(1);
+            PascalCase = args[i].substring(0,1).toUpperCase() + args[i].substring(1).toLowerCase();
             System.out.print(PascalCase);
         }
         System.out.println();
