56d56882d875f07ab8981e71987bf82e4db4e2bf
bertua <151688160+bertua@users.noreply.github.com>
2024-02-20 23:05:16 -0300

    Update Ex07.java

--- lucas/Strings/Ex07.java
+++ lucas/Strings/Ex07.java
@@ -1,7 +1,7 @@
 //Leia uma frase e converta para "Title Case"
 public class Ex07 {
     public static void main(String[] args){
-        String title = "";
+        String Titlecase = "";
         for(int i = 0; i < args.length; i++){
             title = args[i].substring(0,1).toUpperCase() + args[i].substring(1).toLowerCase();
             System.out.print(title);
