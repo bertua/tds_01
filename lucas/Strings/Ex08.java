4c0fd85bbb43adf8c7ac58813433fdf409b044dd
bertua <151688160+bertua@users.noreply.github.com>
2024-02-20 23:13:41 -0300

    Update Ex08.java

--- lucas/Strings/Ex08.java
+++ lucas/Strings/Ex08.java
@@ -12,3 +12,18 @@ public class Ex08 {
         System.out.println();
     }
 }
+
+/*
+public class Ex08 {
+    public static void main(String[] args){
+        String snake_case = "";
+        for(int i = 0; i < args.length; i++){
+            snake_case += args[i].toLowerCase();
+            if(i < args.length -1){
+               snake_case += "_";
+            }
+        }
+        System.out.println(snake_case);
+    }
+}
+*/
