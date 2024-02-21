dbd8e64457d99a3600ccb779998118c5c6226b2c
bertua <151688160+bertua@users.noreply.github.com>
2024-02-21 13:30:36 -0300

    Update Ex05.java

--- lucas/Numeros/Ex05.java
+++ lucas/Numeros/Ex05.java
@@ -2,10 +2,11 @@
 public class Ex05{
     public static void main(String[] args){
         int numero;
+        int fatorial;
         for(int i = 0; i < args.length; i++){
             numero = Integer.parseInt(args[i]);
-            int fatorial = 1;
-            for(int j = 0 ; j < numero; j++){
+            fatorial = 1;
+            for(int j = 0; j < numero; j++){
                 fatorial *= numero - j;
             }
             System.out.println(fatorial);
