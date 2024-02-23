7b58757a67514a9086b8fc49377985c8712bce32
bertua <151688160+bertua@users.noreply.github.com>
2024-02-23 00:06:36 -0300

    Update Ex06.java

--- lucas/Numeros/Ex06.java
+++ lucas/Numeros/Ex06.java
@@ -6,24 +6,21 @@ public class Ex06{
         for(int i = 0; i < args.length; i++){
             numero = Integer.parseInt(args[i]);
 
-            if(numero >= 2) {
-                check = 0;
-                for(int j = 2; j < numero; j++){
-                    if(numero % j == 0){
-                        System.out.println("Não é primo");
-                        check++;
-                        break;
-                    }
-                }
-                if(check == 0){
-                    System.out.println("É primo");
-                }
-                
-            } 
-            else {
+            if(numero < 2){
                 System.out.println("Não é primo");
-            } 
+            }
+            check = 0;
+            for(int j = 2; j < numero; j++){
+                if(numero % j == 0){
+                    System.out.println("Não é primo");
+                    check++;
+                    break;
+                }
+            }
+            if(check == 0){
+                System.out.println("É primo");
+            }
+
         }
     }
 }
-                
