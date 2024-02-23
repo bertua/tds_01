98af1f86f7153a4e745882eeb875777a9c11fcee
bertua <151688160+bertua@users.noreply.github.com>
2024-02-23 12:37:49 -0300

    Update PosicaoEspecifica.java

--- lucas/Scanner/PosicaoEspecifica.java
+++ lucas/Scanner/PosicaoEspecifica.java
@@ -3,8 +3,7 @@ public class PosicaoEspecifica{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         String nome = input.nextLine();
-        String posicao = args[0];
-        int posicaoInt = Integer.parseInt(posicao);
-        System.out.println(nome.charAt(posicaoInt));
+        int posicao = input.nextInt();
+        System.out.println(nome.charAt(posicao));
     }
 }
