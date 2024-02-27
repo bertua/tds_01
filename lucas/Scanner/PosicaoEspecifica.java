75de0d8aa7efac81136cc446dab04109f20e3d16
Lucas Bertuol <151688160+bertua@users.noreply.github.com>
2024-02-27 02:45:23 +0000

    rm

--- lucas/Scanner/PosicaoEspecifica.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class PosicaoEspecifica{
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        String nome = input.nextLine();
-        int posicao = input.nextInt();
-        System.out.println(nome.charAt(posicao));
-    }
-}
