111c67aa9bd8d12a517e1ce278d47d46c740bc83
Lucas Bertuol <151688160+bertua@users.noreply.github.com>
2024-03-18 05:01:23 +0000

    formatar

--- lucas/Pizzaria-v0.2/Pedido.java
+++ lucas/Pizzaria-v0.2/Pedido.java
@@ -22,18 +22,30 @@ public class Pedido{
     public void imprimir(){
         System.out.println("----------------------------------------");
         System.out.println("                 Pizzaria               ");
-        System.out.println("                 Bate-Papo              ");
+        System.out.println("                 Luigi's                ");
         System.out.println("----------------------------------------");
-        System.out.println("Cliente     : " + this.cliente);
+        System.out.println(formatar("Cliente", this.cliente));
         System.out.println("----------------------------------------");
-        System.out.println("Tipo        : " + this.itemDoPedido.getTipo());
-        System.out.println("Sabor       : " + this.itemDoPedido.getSabor());
-        System.out.println("Valor       : " + this.itemDoPedido.getValor());
+        System.out.println(formatar("Tipo", this.itemDoPedido.getTipo()));
+        System.out.println(formatar("Sabor", this.itemDoPedido.getSabor()));
+        System.out.println(formatar("Valor", String.valueOf(this.itemDoPedido.getValor())));
         System.out.println("----------------------------------------");
-        System.out.println("Tx. Entrega : " + this.taxaEntrega);
+        System.out.println(formatar("Tx. Entrega", String.valueOf(this.taxaEntrega)));
         System.out.println("----------------------------------------");
-        System.out.println("Total       : " + this.getTotal());
+        System.out.println(formatar("Total", String.valueOf(this.getTotal())));
         System.out.println("----------------------------------------");
     }
 
+    private static String formatar(String titulo, String item){
+        String div = "----------------------------------------";
+        int finalDiv = div.length() -1;
+        int finalItem = titulo.length() + item.length() -1;
+
+        while(finalDiv != finalItem && item.length() < div.length()){
+            item = " " + item;
+            finalItem = titulo.length() + item.length() -1;
+
+        }
+        return titulo + item;
+    }
 }
